cc8fd25cb62b6f84e5257927df56aee025167ee3
“selpiiee” <selvynurkhoffifa@gmail.com>
2026-06-26 17:54:55 +0700

    Item Node Class

---
+++ iNode.java
@@ -0,0 +1,11 @@
+public class iNode {
+    items item;
+    iNode left;
+    iNode right;
+
+    public iNode(items item) {
+        this.item = item;
+        this.left = null;
+        this.right = null;
+    }
+}
